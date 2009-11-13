9b5fc4a493212d7f322eca6585de899c48c6bec0
Andrew Hyatt <ahyatt@ahyatt-macbookpro.(none)>
2009-11-12 22:32:44 -0500

    Initial checkin

---
+++ sdmbnick.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from google.appengine.ext import db
+
+from waveapi import events
+from waveapi import model
+from waveapi import robot
+
+import logging
+import re
+
+class SdmbUser(db.Model):
+    address = db.StringProperty(required=True)
+    nick = db.StringProperty(required=True)
+
+    def __str__(self):
+        return "SdmbUser{address: %s, nick: %s}" % (self.address, self.nick)
+
+
+class NotificationsRobot(robot.Robot):
+    def __init__(self):
+        robot.Robot.__init__(self, "sdmbnicky",
+                             version=1,
+                             image_url="http://sdmbnicky.appspot.com/favicon.png",
+                             profile_url='http://sdmbnicky.appspot.com/')
+        self.RegisterListener(self)
+
+    def on_wavelet_participants_changed(self, event, context):
+        pass
+
+    def on_blip_submitted(self, event, context):
+        """Invoked when a blip is submitted.
+
+        Here we do two things: check for commands, and markup new
+        blips."""
+        blip = context.GetBlipById(event['blipId'])
+        text = blip.GetDocument().GetText().lower()
+        logging.debug("Matching text: %s" % str(text))
+        m = re.match("^My nick is (.*)$", text, re.I)
+        if m:
+            user = SdmbUser(address=blip.creator,
+                            nick=m.group(1).strip())
+            logging.debug("Creating new SdmbUser: %s" % str(user));
+            user.put()
+        nick = self.lookup(blip.creator)
+        logging.debug("Looking up: %s" % str(blip.creator))
+        if nick:
+            logging.debug("Found: %s" % str(nick))
+            blip = context.GetBlipById(event['blipId'])
+            blip.GetDocument().SetText("[%s]: %s" %
+                                       (nick, blip.GetDocument().GetText()))
+
+    def lookup(self, wave_user):
+        """Lookup the nick of the wave user passed in.
+
+        If there is none found, return None."""
+        query = SdmbUser.all()
+        query.filter("address =", wave_user)
+        results = query.fetch(1)
+        if len(results) == 0:
+            return None
+        else:
+            return results[0].nick
+
+if __name__ == '__main__':
+    NotificationsRobot().Run()
